1eab999fd0aab59a587b8bc50ec687b928ae8d9b
Anna Saenko <asaenko@c2r4s4.42luxembourg.lu>
2024-02-26 15:23:20 +0100

    added solutions to memmove and strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaenko <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 15:05:49 by asaenko           #+#    #+#             */
+/*   Updated: 2024/02/26 15:05:52 by asaenko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stddrf.h>
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+size_t	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	size_t	i;
+	size_t	j;
+
+	if (size <= ft_strlen(dest))
+	{
+		return (size + ft_strlen(src));
+	}
+	i = ft_strlen(dest);
+	j = 0;
+	while (src[j] != '\0' && i + 1 < size)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(dest) + ft_strlen(&src[j]));
+}
--- libft.h
+++ libft.h
@@ -23,5 +23,6 @@ size_t	ft_strlen(char *str);
 void	*ft_memset(void *dest, int ch, size_t count);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t count);
+void	*ft_memmove(void *dest, const void *src, size_t count);
 
 #endif

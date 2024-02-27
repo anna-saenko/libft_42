7af516ba2feb2986a1c0a7043639dd2d113e8357
Anna Saenko <asaenko@c1r4s4.42luxembourg.lu>
2024-02-27 14:02:42 +0100

    added ft_strrchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,4 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (*s);
+		}
+		*s++
+	}
+	return (0);
+}

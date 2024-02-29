ceaf726489c078a5cbb1ff7e975a5f1ae05b333e
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2024-02-29 11:34:44 +0100

    added ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+#include <stddef.h>
+
+void	*ft_memchr(const void *ptr, int ch, size_t count)
+{
+	unsigned char	*ptr_cpy;
+	unsigned char	c;
+	size_t			i;
+
+	ptr_cpy = (unsigned char *)ptr;
+	c = (unsigned char)ch;
+	i = 0;
+	while (i < count)
+	{
+		if (ptr_cpy[i] == c)
+		{
+			return (ptr_cpy + i);
+		}
+		i++;
+	}
+	return (NULL);
+}

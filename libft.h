f32b3af59ad55284c0a25c2e6e569c9828f469fb
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2024-03-01 16:48:06 +0100

    added library includes

--- libft.h
+++ libft.h
@@ -13,6 +13,8 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -25,11 +27,18 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t count);
 void	*ft_memmove(void *dest, const void *src, size_t count);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlcat(char *dest, char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 int		ft_toupper(int ch);
 int		ft_tolower(int ch);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int		strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *ptr, int ch, size_t count);
+void	*ft_memmove(void *dest, const void *src, size_t count);
+int	ft_memcmp(const void *lhs, const void *rhs, size_t count);
+char	*ft_strnstr(const char *s1, const char *s2, size_t len);
+int	ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *str);
 
 #endif

69be64695233a845e08836fddc9cb861e5a55135
Anna Saenko <61340209+feelfaque@users.noreply.github.com>
2024-03-02 16:26:04 +0100

    added ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -11,19 +11,27 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
-/*
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
-{
-	char		*dest_cpy;
-	const char	*src_cpy;
-	size_t		i;
 
-	if (i != 0)
+size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
+{
+	size_t	len;
+	char	*src_cpy;
+
+	len = 0;
+	src_cpy = (char *)src;
+	if (!dest)
 	{
-		while (n--)
-		{
-			if()
-		}
+		return (0);
 	}
-}*/
+	len = ft_strlen(src_cpy);
+	if (!destsize)
+	{
+		return (len);
+	}
+	while (--destsize && *src_cpy)
+	{
+		*dest++ = *src_cpy++;
+	}
+	*dest = '\0';
+	return (len);
+}

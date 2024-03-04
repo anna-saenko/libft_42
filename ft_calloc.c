1709ded12d961afe33bec3cd1ead2d554140bcb3
Anna Saenko <a.saenko2109@gmail.com>
2024-03-04 22:42:59 +0100

    fixed memory leaks in calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -4,14 +4,15 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*arr;
 
-	if (size == SIZE_MAX || count == SIZE_MAX || size * count == 0)
-		return (void *)malloc(0);
 	if ((size == SIZE_MAX && count == SIZE_MAX) ||
 		(size != 0 && count >= SIZE_MAX / size) || size * count == SIZE_MAX)
 		return (NULL);
+	if (size == SIZE_MAX || count == SIZE_MAX || size * count == 0)
+		return (void *)malloc(0);
 	arr = (void *)malloc(size * count);
 	if (!arr)
 	{
+		free(arr);
 		return (NULL);
 	}
 	ft_bzero(arr, count * size);

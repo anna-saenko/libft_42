57e1b0dd9883f753513e676c041c146b10b647d7
Anna Saenko <a.saenko2109@gmail.com>
2024-03-28 14:07:40 +0100

    fixed bugs

--- ft_tolower.c
+++ ft_tolower.c
@@ -16,7 +16,7 @@ int	ft_tolower(int ch)
 {
 	if (ch >= 65 && ch <= 90)
 	{
-		return (ch += 32);
+		return (ch + 32);
 	}
 	return (ch);
 }
--- ft_toupper.c
+++ ft_toupper.c
@@ -16,7 +16,7 @@ int	ft_toupper(int ch)
 {
 	if (ch >= 97 && ch <= 122)
 	{
-		return (ch -= 32);
+		return (ch - 32);
 	}
 	return (ch);
 }
